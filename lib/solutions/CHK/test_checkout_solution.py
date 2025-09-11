10ec1a8ad719ee5c1326781045b52a78293dea57
sbx_user1051 <sbx_user1051@169.254.63.149>
2025-09-11 21:30:53 +0000

    Thu Sep 11 21:30:53 UTC 2025

--- lib/solutions/CHK/test_checkout_solution.py
+++ lib/solutions/CHK/test_checkout_solution.py
@@ -59,10 +59,11 @@ class MyTestCase(unittest.TestCase):
 
     def test_group_offers(self):
         solution = CheckoutSolution()
-        response = solution.checkout("XYZYYYYYYT")
+        response = solution.checkout("XYZYYYT")
         assert response == 110
 
 
 if __name__ == '__main__':
     unittest.main()
+
 

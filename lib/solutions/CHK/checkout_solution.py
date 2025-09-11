6d9beddbf1035057e538b8ec34de6245431fcf6f
sbx_user1051 <sbx_user1051@169.254.63.149>
2025-09-11 16:17:01 +0000

    Thu Sep 11 16:17:01 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -48,6 +48,7 @@ class CheckoutSolution:
             return item_count * self.prices[item]
 
     def _reprocess_item(self, sku:str):
-        price_with_free = self._calculate_cost(sku, self.total[sku]["quantity"] - self.reprocess[sku])
-        total_before = self.total[sku]["price"]
-        self.total[sku]["price"] = price_with_free if total_before > price_with_free else price_with_free
+        if self.total[sku]["quantity"]:
+            price_with_free = self._calculate_cost(sku, self.total[sku]["quantity"] - self.reprocess[sku])
+            total_before = self.total[sku]["price"]
+            self.total[sku]["price"] = price_with_free if total_before > price_with_free else price_with_free

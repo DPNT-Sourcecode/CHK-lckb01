789b7e295cff6c16c1c739ab63e7fd23d0566a03
sbx_user1051 <sbx_user1051@169.254.63.149>
2025-09-11 20:05:41 +0000

    Thu Sep 11 20:05:41 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,17 +6,17 @@ class CheckoutSolution:
     def __init__(self):
         self.prices = {"A": 50, "B": 30, "C": 20, "D": 15,
                        "E": 40, "F": 10, "G": 20, "H": 10,
-                       "I": 35, "J": 60, "K": 80, "L": 90,
+                       "I": 35, "J": 60, "K": 70, "L": 90,
                        "M": 15, "N": 40, "O": 10, "P": 50,
-                       "Q": 30, "R": 50, "S": 30, "T": 20,
-                       "U": 40, "V": 50, "W": 20, "X": 90,
-                       "Y": 10, "Z": 50}
+                       "Q": 30, "R": 50, "S": 20, "T": 20,
+                       "U": 40, "V": 50, "W": 20, "X": 17,
+                       "Y": 20, "Z": 21}
         self.offers = {"A":[{"quantity": 5, "price": 200}, {"quantity": 3, "price": 130}],
                        "B": [{"quantity": 2, "price": 45}],
                        "E": [{"quantity": 2, "price": 80, "free": "B"}],
                        "F": [{"quantity": 3, "price": 20}],
                        "H": [{"quantity": 10, "price": 80}, {"quantity": 5, "price": 45}],
-                       "K": [{"quantity": 2, "price": 150}],
+                       "K": [{"quantity": 2, "price": 120}],
                        "N": [{"quantity": 3, "price": 120, "free": "M"}],
                        "P": [{"quantity": 5, "price": 200}],
                        "Q": [{"quantity": 3, "price": 80}],
@@ -26,10 +26,12 @@ class CheckoutSolution:
                        }
         self.total = {}
         self.reprocess = {}
+        self.group_offers = {"letters":"STXYZ", "cost": 45}
     # skus = unicode string
     def checkout(self, skus:str):
         total = 0
         skus = "".join(sorted(skus))
+        skus = self._remove_group_offers_from_skus(skus)
         for sku in self.prices.keys():
             sku_count = skus.count(sku)
             self.total[sku] = dict(quantity=sku_count, price= self._calculate_cost(sku, sku_count) if sku_count > 0 else 0)
@@ -68,3 +70,27 @@ class CheckoutSolution:
             price_with_free = self._calculate_cost(sku, self.total[sku]["quantity"] - self.reprocess[sku])
             total_before = self.total[sku]["price"]
             self.total[sku]["price"] = price_with_free if total_before > price_with_free else price_with_free
+
+    def _remove_group_offers_from_skus(self, skus:str):
+        letters_to_be_removed = []
+        count_per_letter = self._get_count_of_each_letter_in_group_offers(skus)
+
+        return skus
+
+    def _get_count_of_each_letter_in_group_offers(self, skus:str):
+        count_per_letter = []
+        for letter in self.group_offers.get("letters", ""):
+            count = skus.count(letter)
+            if count:
+                count_per_letter.append((count, letter))
+        return count_per_letter.sort(reverse=True)
+
+    def list_of_number_of_group_offers_per_letter(self, count_per_letter:list):
+        list_of_letters_to_remove = []
+        length_of_letter_count = len(count_per_letter)
+        if length_of_letter_count < 3:
+            return list_of_letters_to_remove
+        elif length_of_letter_count == 3:
+            count_of_offers = count_per_letter[-1]
+            
+

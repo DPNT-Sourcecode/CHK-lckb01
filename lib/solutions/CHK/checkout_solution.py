8abf857eb5f04d342bd56bf9b715f9c3fbde2bb0
sbx_user1051 <sbx_user1051@169.254.63.149>
2025-09-11 18:27:06 +0000

    Thu Sep 11 18:27:06 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,7 +4,13 @@ from math import floor
 class CheckoutSolution:
 
     def __init__(self):
-        self.prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10, "G": 20, "H": 10, "I": 35, "J": 60}
+        self.prices = {"A": 50, "B": 30, "C": 20, "D": 15,
+                       "E": 40, "F": 10, "G": 20, "H": 10,
+                       "I": 35, "J": 60, "K": 80, "L": 90,
+                       "M": 15, "N": 40, "O": 10, "P": 50,
+                       "Q": 30, "R": 50, "S": 30, "T": 20,
+                       "U": 40, "V": 50, "W": 20, "X": 90,
+                       "Y": 10, "Z": 50}
         self.offers = {"A":[{"quantity": 5, "price": 200}, {"quantity": 3, "price": 130}],
                        "B": [{"quantity": 2, "price": 45}],
                        "E": [{"quantity": 2, "price": 80, "free": "B"}],
@@ -53,4 +59,5 @@ class CheckoutSolution:
             price_with_free = self._calculate_cost(sku, self.total[sku]["quantity"] - self.reprocess[sku])
             total_before = self.total[sku]["price"]
             self.total[sku]["price"] = price_with_free if total_before > price_with_free else price_with_free
+
 

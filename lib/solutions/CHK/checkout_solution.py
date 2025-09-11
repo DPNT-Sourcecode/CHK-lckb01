a89d94ddd25e5aef75c2b81b69394f73dc1e54ff
sbx_user1051 <sbx_user1051@169.254.63.149>
2025-09-11 14:23:57 +0000

    Thu Sep 11 14:23:57 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,14 @@
 
 class CheckoutSolution:
 
+    def __init__(self):
+        self.prices = {"A": 50, "B": 45, "C": 35, "D": 20}
+        self.offers = {"A":{"quantity": 3, "price": 130}, "B": {"quantity": 2, "price": 45}}
     # skus = unicode string
-    def checkout(self, skus):
-        raise NotImplementedError()
+    def checkout(self, skus:str):
+        while skus:
+            skus.count("A")
+            skus.count("B")
+
+    def _calculate_cost(self, item_count:int, item: str):
+        return 
